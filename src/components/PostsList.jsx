623861ead2c34b6f13d6efd00aa0dfdd5c8fbc0f
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-09 17:52:50 +0200

    feat: add modal conditionally  rendering

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,24 +2,40 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css'
 import { useState } from 'react';
+import Modal from "./Modal";
 
 
 
 export default function PostsList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler(){
+        setModalIsVisible(false);
+    }
 
     function changeBodyHandler(event) {
         setEnteredBody(event.target.value);
     }
-    
+
     function changeAuthorHandler(event) {
         setEnteredAuthor(event.target.value);
     }
 
+    let modalContent;
+
+    if(modalIsVisible){
+        modalContent = (
+            <Modal onClose={hideModalHandler}>
+                <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+            </Modal>
+        )
+    };
+
     return (
         <>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+            {modalContent}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Manuel" body="Check out the full course!" />
